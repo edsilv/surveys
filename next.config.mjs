3a469c276117a9c091e5e9fd773d6418a6ef4d87
Edward Silverton <e.silverton@gmail.com>
2025-11-20 11:34:31 +0000

    config

--- next.config.mjs
+++ next.config.mjs
@@ -4,23 +4,6 @@ import { withPayload } from '@payloadcms/next/withPayload';
 const nextConfig = {
   // Your Next.js config here
   cacheComponents: true,
-  webpack: (webpackConfig, { webpack }) => {
-    webpackConfig.resolve.extensionAlias = {
-      '.cjs': ['.cts', '.cjs'],
-      '.js': ['.ts', '.tsx', '.js', '.jsx'],
-      '.mjs': ['.mts', '.mjs'],
-    };
-
-    // Ignore test directories in node_modules
-    webpackConfig.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /\/test\//,
-        contextRegExp: /node_modules/,
-      }),
-    );
-
-    return webpackConfig;
-  },
 };
 
 export default withPayload(nextConfig, { devBundleServerPackages: false });
