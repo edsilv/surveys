59800a51e21e7397a9e2efbe623b584888bde7a2
Edward Silverton <e.silverton@gmail.com>
2025-11-20 11:51:52 +0000

    externals

--- next.config.mjs
+++ next.config.mjs
@@ -4,20 +4,7 @@ import { withPayload } from '@payloadcms/next/withPayload';
 const nextConfig = {
   // Your Next.js config here
   cacheComponents: true,
-  webpack: (config, { webpack }) => {
-    // Ignore thread-stream test directory completely
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^tap$|^why-is-node-running$|^pino-elasticsearch$/,
-      }),
-      new webpack.IgnorePlugin({
-        resourceRegExp: /\/test\//,
-        contextRegExp: /thread-stream/,
-      }),
-    );
-
-    return config;
-  },
+  serverExternalPackages: ['pino', 'thread-stream'],
 };
 
 export default withPayload(nextConfig, { devBundleServerPackages: false });
