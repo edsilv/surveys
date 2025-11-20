cbd9277dd383ecb51c915a29721648f950fad29a
Edward Silverton <e.silverton@gmail.com>
2025-11-20 11:54:05 +0000

    resolver

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,18 @@ const nextConfig = {
   // Your Next.js config here
   cacheComponents: true,
   serverExternalPackages: ['pino', 'thread-stream'],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Stub out test dependencies that thread-stream's test files try to require
+      config.resolve.alias = {
+        ...config.resolve.alias,
+        'why-is-node-running': false,
+        tap: false,
+        'pino-elasticsearch': false,
+      };
+    }
+    return config;
+  },
 };
 
 export default withPayload(nextConfig, { devBundleServerPackages: false });
